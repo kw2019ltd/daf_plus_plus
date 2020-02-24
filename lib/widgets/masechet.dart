ffe24c7a28decb76bdd482959c4b47034748f32e
zvikarp <zvi@zektec.com>
2020-02-24 07:22:04 +0200

    made the masechet card clickable closes #7

--- lib/widgets/masechet.dart
+++ lib/widgets/masechet.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 class MasechetWidget extends StatelessWidget {
-
   MasechetWidget({
     @required this.masechetName,
     @required this.isExpanded,
@@ -16,32 +15,39 @@ class MasechetWidget extends StatelessWidget {
   final Function(bool) onChangeExpanded;
   final double progressInPecent;
 
+  void _changeExpandedState() {
+    this.onChangeExpanded(!this.isExpanded);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: Colors.blue,
-      ),
-      child: Row(
-        children: <Widget>[
-          Transform.rotate(
-            angle: this.isExpanded ? pi / 1 : 0,
-            child: IconButton(
-              icon: Icon(Icons.keyboard_arrow_down),
-              onPressed: () => this.onChangeExpanded(!this.isExpanded),
+    return GestureDetector(
+      onTap: _changeExpandedState,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.blue,
+        ),
+        child: Row(
+          children: <Widget>[
+            Transform.rotate(
+              angle: this.isExpanded ? pi / 1 : 0,
+              child: IconButton(
+                icon: Icon(Icons.keyboard_arrow_down),
+                onPressed: _changeExpandedState,
+              ),
             ),
-          ),
-          Expanded(child: Text(Consts.MASECHET_TITLE + " " + masechetName)),
-          CircularProgressIndicator(
-            // backgroundColor: Colors.white,
-            value: this.progressInPecent,
-            strokeWidth: 3,
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-          ),
-        ],
+            Expanded(child: Text(Consts.MASECHET_TITLE + " " + masechetName)),
+            CircularProgressIndicator(
+              // backgroundColor: Colors.white,
+              value: this.progressInPecent,
+              strokeWidth: 3,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }

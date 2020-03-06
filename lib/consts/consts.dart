9f7d897385c8120f9628568e525173b83e284b36
zvikarp <zvi@zektec.com>
2020-03-06 12:14:58 +0200

    changed storage type to a list of numbers

--- lib/consts/consts.dart
+++ lib/consts/consts.dart
@@ -4,7 +4,8 @@ class Consts {
   static const double MASECHET_LIST_HEIGHT = 400;
 
   static const int FIST_DAF = 1;
-  static const int MAX_REVISIONS = 20;
+  static const String PROGRESS_DIVIDER = ',';
+  static const int MAX_REVISIONS = 10000;
 
   static const List<Locale> LOCALES = [Locale("he", "IL"), Locale("en", "US")];
   static const Locale DEFAULT_LOCALE = Locale("he", "IL");

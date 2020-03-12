64b8306d3ccafbca9b4f49e63a1ca81a3ef8b2b5
chaimgross <chaimgross1026@gmail.com>
2020-03-12 20:15:03 +0200

    Save filled in Mesechtas for daf yomi

--- lib/dialogs/firstUseDialogTwo.dart
+++ lib/dialogs/firstUseDialogTwo.dart
@@ -1,25 +1,53 @@
-import 'package:flutter/material.dart';
-
+import 'package:daf_plus_plus/actions/progress.dart';
+import 'package:daf_plus_plus/consts/consts.dart';
+import 'package:daf_plus_plus/data/masechets.dart';
+import 'package:daf_plus_plus/dialogs/FirstUseDialogFillIn.dart';
 import 'package:daf_plus_plus/models/dafLocation.dart';
+import 'package:daf_plus_plus/models/masechet.dart';
+import 'package:daf_plus_plus/pages/home.dart';
+import 'package:daf_plus_plus/services/hive/index.dart';
 import 'package:daf_plus_plus/stores/dafsDates.dart';
 import 'package:daf_plus_plus/utils/dateConverter.dart';
-import 'package:daf_plus_plus/dialogs/FirstUseDialogFillIn.dart';
-import 'package:daf_plus_plus/consts/consts.dart';
 import 'package:daf_plus_plus/utils/gematriaConverter.dart';
+import 'package:daf_plus_plus/utils/localization.dart';
+import 'package:daf_plus_plus/utils/masechetConverter.dart';
+import 'package:daf_plus_plus/utils/transparentRoute.dart';
 import 'package:daf_plus_plus/widgets/core/button.dart';
 import 'package:daf_plus_plus/widgets/core/dialog.dart';
 import 'package:daf_plus_plus/widgets/core/title.dart';
-import 'package:daf_plus_plus/utils/localization.dart';
-import 'package:daf_plus_plus/pages/home.dart';
-import 'package:daf_plus_plus/services/hive/index.dart';
-import 'package:daf_plus_plus/utils/transparentRoute.dart';
+import 'package:flutter/material.dart';
 
 class FirstUseDialogTwo extends StatelessWidget {
   _yes(BuildContext context) {
+    _fillIn();
     hiveService.settings.setHasOpened(true);
     Navigator.pushReplacement(context,
         MaterialPageRoute(builder: (BuildContext context) => HomePage()));
-    // TODO Set all daf done till current
+  }
+
+  _fillIn() {
+    DafLocationModel todaysDaf =
+    dafsDatesStore.getDafByDate(dateConverterUtil.getToday());
+    int mesechta = MasechetsData.THE_MASECHETS
+        .firstWhere((element) => element.id == todaysDaf.masechetId)
+        .index;
+    if (mesechta > 0) {
+      for (int i = 0; i < mesechta; i++) {
+        MasechetModel masechetModel = MasechetsData.THE_MASECHETS[i];
+        List<int> progressMap = List.filled(masechetModel.numOfDafs, 1);
+        String progressString =
+        masechetConverterUtil.encode(progressMap.map((daf) => 1).toList());
+        progressAction.update(masechetModel.id, progressString);
+      }
+    }
+    MasechetModel currentMasechetModel = MasechetsData.THE_MASECHETS[mesechta];
+    List<int> dafMap = List.filled(currentMasechetModel.numOfDafs, 0);
+    for (int i = 0; i < todaysDaf.dafIndex; i++) {
+      dafMap[i] = 1;
+    }
+    String progressString =
+    masechetConverterUtil.encode(dafMap.map((daf) => daf).toList());
+    progressAction.update(currentMasechetModel.id, progressString);
   }
 
   _no(BuildContext context) {
@@ -40,7 +68,7 @@ class FirstUseDialogTwo extends StatelessWidget {
 
   String getYesterdaysDaf() {
     DateTime yesterday =
-        dateConverterUtil.getToday().subtract(Duration(days: -1));
+    dateConverterUtil.getToday().subtract(Duration(days: 1));
     DafLocationModel yesterdaysDaf = dafsDatesStore.getDafByDate(yesterday);
 
     String masechet = localizationUtil.translate(yesterdaysDaf.masechetId);

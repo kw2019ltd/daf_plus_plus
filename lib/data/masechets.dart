4ae96b75922896c27806c245e1ffdb806f872c22
zvikarp <zvi@zektec.com>
2020-03-12 11:40:57 +0200

    removed hard coded stuff, closes #46

--- lib/data/masechets.dart
+++ lib/data/masechets.dart
@@ -7,7 +7,8 @@ class MasechetsData {
     MasechetModel(index: 2, numOfDafs: 104, sederId: "moed", id: "eruvin"),
     MasechetModel(index: 3, numOfDafs: 120, sederId: "moed", id: "pesachim"),
     MasechetModel(index: 4, numOfDafs: 21, sederId: "moed", id: "shekalim"),
-    MasechetModel(index: 5, numOfDafs: 34, sederId: "moed", id: "rosh-hashanah"),
+    MasechetModel(
+        index: 5, numOfDafs: 34, sederId: "moed", id: "rosh-hashanah"),
     MasechetModel(index: 6, numOfDafs: 87, sederId: "moed", id: "yoma"),
     MasechetModel(index: 7, numOfDafs: 55, sederId: "moed", id: "sukkah"),
     MasechetModel(index: 8, numOfDafs: 39, sederId: "moed", id: "beitzah"),
@@ -22,25 +23,34 @@ class MasechetsData {
     MasechetModel(index: 17, numOfDafs: 48, sederId: "nashim", id: "sota"),
     MasechetModel(index: 18, numOfDafs: 89, sederId: "nashim", id: "gittin"),
     MasechetModel(index: 19, numOfDafs: 81, sederId: "nashim", id: "kedushin"),
-    MasechetModel(index: 20, numOfDafs: 118, sederId: "nizikin", id: "bava-kama"),
-    MasechetModel(index: 21, numOfDafs: 118, sederId: "nizikin", id: "bava-metziah"),
-    MasechetModel(index: 22, numOfDafs: 175, sederId: "nizikin", id: "bava-basra"),
-    MasechetModel(index: 23, numOfDafs: 112, sederId: "nizikin", id: "sanhedrin"),
+    MasechetModel(
+        index: 20, numOfDafs: 118, sederId: "nizikin", id: "bava-kama"),
+    MasechetModel(
+        index: 21, numOfDafs: 118, sederId: "nizikin", id: "bava-metziah"),
+    MasechetModel(
+        index: 22, numOfDafs: 175, sederId: "nizikin", id: "bava-basra"),
+    MasechetModel(
+        index: 23, numOfDafs: 112, sederId: "nizikin", id: "sanhedrin"),
     MasechetModel(index: 24, numOfDafs: 23, sederId: "nizikin", id: "makkos"),
     MasechetModel(index: 25, numOfDafs: 48, sederId: "nizikin", id: "shavuos"),
-    MasechetModel(index: 26, numOfDafs: 75, sederId: "nizikin", id: "avoda-zara"),
+    MasechetModel(
+        index: 26, numOfDafs: 75, sederId: "nizikin", id: "avoda-zara"),
     MasechetModel(index: 27, numOfDafs: 13, sederId: "nizikin", id: "horiyos"),
-    MasechetModel(index: 28, numOfDafs: 119, sederId: "kedashim", id: "zevachim"),
-    MasechetModel(index: 29, numOfDafs: 109, sederId: "kedashim", id: "menuchos"),
-    MasechetModel(index: 30, numOfDafs: 141, sederId: "kedashim", id: "chullin"),
-    MasechetModel(index: 31, numOfDafs: 60, sederId: "kedashim", id: "bechoros"),
+    MasechetModel(
+        index: 28, numOfDafs: 119, sederId: "kedashim", id: "zevachim"),
+    MasechetModel(
+        index: 29, numOfDafs: 109, sederId: "kedashim", id: "menuchos"),
+    MasechetModel(
+        index: 30, numOfDafs: 141, sederId: "kedashim", id: "chullin"),
+    MasechetModel(
+        index: 31, numOfDafs: 60, sederId: "kedashim", id: "bechoros"),
     MasechetModel(index: 32, numOfDafs: 33, sederId: "kedashim", id: "erechin"),
     MasechetModel(index: 33, numOfDafs: 33, sederId: "kedashim", id: "temora"),
     MasechetModel(index: 34, numOfDafs: 27, sederId: "kedashim", id: "krisos"),
     MasechetModel(index: 35, numOfDafs: 20, sederId: "kedashim", id: "meilah"),
     MasechetModel(index: 36, numOfDafs: 3, sederId: "kedashim", id: "kinnim"),
-    MasechetModel(index: 37, numOfDafs: 4, sederId: "kedashim", id: "mindexos"),
-    MasechetModel(index: 38, numOfDafs: 9, sederId: "kedashim", id: "tamindex"),
-    MasechetModel(index: 39, numOfDafs: 72, sederId: "teharos", id: "nindexdah"),
+    MasechetModel(index: 37, numOfDafs: 4, sederId: "kedashim", id: "midos"),
+    MasechetModel(index: 38, numOfDafs: 9, sederId: "kedashim", id: "tamid"),
+    MasechetModel(index: 39, numOfDafs: 72, sederId: "teharos", id: "niddah"),
   ];
 }

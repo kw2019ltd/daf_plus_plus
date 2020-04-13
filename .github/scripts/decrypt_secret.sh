43344e54dfc4a5dbfce6553b6f61d35b4d4ae035
zvikarp <zvi@zektec.com>
2020-04-13 06:42:19 +0300

    test 6

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,6 +5,6 @@ mkdir $HOME/secrets
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output $HOME/secrets/secrets.tar secrets.tar.gpg
 tar xvf $HOME/secrets/secrets.tar
-ls $HOME/secrets
-ls $HOME/android/app
-tar somthing
+# ls $HOME/secrets
+ls ./android/app
+# tar somthing

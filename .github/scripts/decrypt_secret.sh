e94b4c851d88eb58eeff47c62143c9f1f9e93a6d
zvikarp <zvi@zektec.com>
2020-04-13 06:01:29 +0300

    my bad

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output $HOME/secrets/secrets.tar secrets.tar.gpg
+--output $HOME/secrets.tar secrets.tar.gpg

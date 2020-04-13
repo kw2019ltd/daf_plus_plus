e02c79f7abc471cbec8d82c645c3c2c4526e61e8
zvikarp <zvi@zektec.com>
2020-04-13 06:36:44 +0300

    test 4

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,5 +2,7 @@
 
 # Decrypt the file
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output secrets.tar secrets.tar.gpg
-tar xvf  secrets.tar
+--output $HOME/daf_plus_plus/daf_plus_plus/secrets.tar secrets.tar.gpg
+tar xvf  $HOME/daf_plus_plus/daf_plus_plus/secrets.tar
+ls $HOME/daf_plus_plus/daf_plus_plus/android/app
+tar somthing

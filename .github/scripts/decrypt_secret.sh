e1f1976048a278a1605370a6617d4ad76b886839
zvikarp <zvi@zektec.com>
2020-04-13 06:08:18 +0300

    thats right

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,4 +3,4 @@
 # Decrypt the file
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output $HOME/secrets.tar secrets.tar.gpg
-tar xvf secrets.tar
+tar xvf  $HOME/secrets.tar

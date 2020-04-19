ef023e4126343d6fbf6cb2f33161a1562a95c25f
zvikarp <zvi@zektec.com>
2020-04-19 03:42:58 +0300

    changed name to english

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'דף++',
+      title: 'Daf++',
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,

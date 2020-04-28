7d197585325a227fc3ccf52833fc58d0226f9343
zvikarp <zvi@zektec.com>
2020-04-28 22:16:12 +0300

    added sliding widget #83

--- lib/widgets/home/dafYomiFab.dart
+++ lib/widgets/home/dafYomiFab.dart
@@ -7,7 +7,6 @@ import 'package:daf_plus_plus/services/hive/index.dart';
 import 'package:daf_plus_plus/stores/dafsDates.dart';
 import 'package:daf_plus_plus/utils/dateConverter.dart';
 import 'package:daf_plus_plus/utils/localization.dart';
-import 'package:daf_plus_plus/utils/toast.dart';
 import 'package:daf_plus_plus/utils/transparentRoute.dart';
 import 'package:daf_plus_plus/widgets/core/infoDialog.dart';
 
@@ -18,7 +17,20 @@ import 'package:daf_plus_plus/widgets/core/infoDialog.dart';
 // 4. an undo option in the toast (maybe snackbar)
 // 5. if not doing the daf yomi, dont show
 
-class DafYomiFabWidget extends StatelessWidget {
+class DafYomiFabWidget extends StatefulWidget {
+  @override
+  _DafYomiFabWidgetState createState() => _DafYomiFabWidgetState();
+}
+
+class _DafYomiFabWidgetState extends State<DafYomiFabWidget>
+    with SingleTickerProviderStateMixin {
+  bool isOpened = false;
+  double _fabHeight = 56;
+  double _padding = 18;
+  double _gap = 8;
+  double _popupWidth = 0;
+
+  double _popupPadding = 8;
   DafModel _getTodaysDaf() {
     return dafsDatesStore.getDafByDate(dateConverterUtil.getToday());
   }
@@ -41,27 +53,91 @@ class DafYomiFabWidget extends StatelessWidget {
     progress.data[todaysDaf.number] = 1; // TODO: really not ideal.
     progressAction.update(todaysDaf.masechetId, progress);
     hiveService.settings.setLastDaf(todaysDaf);
-    toastUtil.showInformation(localizationUtil.translate('plus_plus_toast'));
   }
 
-  void _checkIfFirstTime(BuildContext context) {
-    bool isFirst = !hiveService.settings.getUsedFab();
-    if (isFirst) {
-      _displayInfo(context);
-      hiveService.settings.setUsedFab(true);
-    } else
-      _learnedTodaysDaf();
+  void _onClick(BuildContext context, double width) async {
+    if (!isOpened) {
+      setState(() => isOpened = !isOpened);
+      bool isFirst = !hiveService.settings.getUsedFab();
+      if (isFirst) {
+        _displayInfo(context);
+        hiveService.settings.setUsedFab(true);
+      } else {
+        _learnedTodaysDaf();
+        _open(width);
+        await Future.delayed(Duration(seconds: 3));
+        _close();
+      }
+      setState(() => isOpened = !isOpened);
+    }
+  }
+
+  void _open(double width) {
+    setState(() {
+      _popupWidth = width - (_fabHeight + _gap + _padding * 2);
+      _popupPadding = _fabHeight + _gap;
+    });
+  }
+
+  void _close() {
+    setState(() {
+      _popupWidth = 0;
+      _popupPadding = _gap;
+    });
+  }
+
+  Widget popout() {
+    return Transform(
+      transform: Matrix4.translationValues(0, _fabHeight, 0),
+      child: AnimatedContainer(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 10.0,
+              spreadRadius: 0.0,
+              offset: Offset(0, 10),
+            )
+          ],
+        ),
+        duration: Duration(milliseconds: 200),
+        curve: Curves.easeOut,
+        margin: EdgeInsets.only(left: _popupPadding),
+        width: _popupWidth,
+        height: _fabHeight,
+        child: ClipRect(
+          child: Center(child: Text(localizationUtil.translate('plus_plus_toast'))),
+        ),
+      ),
+    );
+  }
+
+  Widget button() {
+    return Container(
+      child: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16.0))),
+        onPressed: () => _onClick(context, MediaQuery.of(context).size.width),
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(
+          Icons.check,
+          color: Theme.of(context).textTheme.headline5.color,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () => _checkIfFirstTime(context),
-      backgroundColor: Theme.of(context).primaryColor,
-      child: Icon(
-        Icons.check,
-        color: Theme.of(context).textTheme.headline5.color,
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        popout(),
+        button(),
+      ],
     );
   }
 }

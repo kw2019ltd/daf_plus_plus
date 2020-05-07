86a4b0d39bb5f555f7361acc96fae192da7b0110
zvikarp <zvi@zektec.com>
2020-05-08 00:44:09 +0300

    🎨 updated theme closes #106

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -8,7 +8,7 @@ class ThemeUtil {
       // colors
       brightness: Brightness.light,
       primaryColor: Colors.indigo[500],
-      accentColor: Colors.blueGrey[700],
+      accentColor: Colors.teal[300],
       backgroundColor: Colors.blueGrey[50],
 
       // typography
@@ -16,8 +16,20 @@ class ThemeUtil {
         Theme.of(context).textTheme,
       ).merge(
         TextTheme(
+          button: TextStyle(
+            fontSize: 18,
+          ),
+          bodyText1: TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+          ),
           bodyText2: TextStyle(
             fontSize: 18,
+          ),
+          headline3: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 32,
           ),
           headline5: TextStyle(
             color: Colors.white,
@@ -28,7 +40,7 @@ class ThemeUtil {
             fontSize: 24,
           ),
           caption: TextStyle(
-            fontWeight: FontWeight.bold
+            fontSize: 14,
           ),
         ),
       ),
